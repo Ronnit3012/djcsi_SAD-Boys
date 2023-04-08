a86ed3635647174b340b7c09719d6834b9e90e99
Sairaaj Surve <sairaaj26@gmail.com>
2023-04-08 23:46:48 +0530

    dump

---
+++ deeplab/model.py
@@ -0,0 +1,222 @@
+import os
+from io import StringIO
+import tarfile
+import tempfile
+import urllib
+import cv2
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from os import path
+
+
+class DeepLabModel(object):
+    """Class to load deeplab model and run inference."""
+
+    INPUT_TENSOR_NAME = 'ImageTensor:0'
+    OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
+    INPUT_SIZE = 513
+    FROZEN_GRAPH_NAME = 'frozen_inference_graph'
+
+    def __init__(self, tarball_path):
+        """Creates and loads pretrained deeplab model."""
+        self.graph = tf.Graph()
+
+        graph_def = None
+        # Extract frozen graph from tar archive.
+        tar_file = tarfile.open(tarball_path)
+        for tar_info in tar_file.getmembers():
+            if self.FROZEN_GRAPH_NAME in os.path.basename(tar_info.name):
+                file_handle = tar_file.extractfile(tar_info)
+                graph_def = tf.compat.v1.GraphDef.FromString(
+                    file_handle.read())
+                break
+
+        tar_file.close()
+
+        if graph_def is None:
+            raise RuntimeError('Cannot find inference graph in tar archive.')
+
+        with self.graph.as_default():
+            tf.import_graph_def(graph_def, name='')
+
+        self.sess = tf.compat.v1.Session(graph=self.graph)
+
+    def run(self, image):
+        """Runs inference on a single image.
+
+        Args:
+          image: A PIL.Image object, raw input image.
+
+        Returns:
+          resized_image: RGB image resized from original input image.
+          seg_map: Segmentation map of `resized_image`.
+        """
+        width, height = image.size
+        resize_ratio = 1.0 * self.INPUT_SIZE / max(width, height)
+        target_size = (int(resize_ratio * width), int(resize_ratio * height))
+        resized_image = image.convert('RGB').resize(
+            target_size, Image.ANTIALIAS)
+        batch_seg_map = self.sess.run(
+            self.OUTPUT_TENSOR_NAME,
+            feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]})
+        seg_map = batch_seg_map[0]
+        return resized_image, seg_map
+
+
+def create_pascal_label_colormap():
+    """Creates a label colormap used in PASCAL VOC segmentation benchmark.
+
+    Returns:
+      A Colormap for visualizing segmentation results.
+    """
+    colormap = np.zeros((256, 3), dtype=int)
+    ind = np.arange(256, dtype=int)
+
+    for shift in reversed(range(8)):
+        for channel in range(3):
+            colormap[:, channel] |= ((ind >> channel) & 1) << shift
+        ind >>= 3
+
+    return colormap
+
+
+def label_to_color_image(label):
+    """Adds color defined by the dataset colormap to the label.
+
+    Args:
+      label: A 2D array with integer type, storing the segmentation label.
+
+    Returns:
+      result: A 2D array with floating type. The element of the array
+        is the color indexed by the corresponding element in the input label
+        to the PASCAL color map.
+
+    Raises:
+      ValueError: If label is not of rank 2 or its value is larger than color
+        map maximum entry.
+    """
+    if label.ndim != 2:
+        raise ValueError('Expect 2-D input label')
+
+    colormap = create_pascal_label_colormap()
+
+    if np.max(label) >= len(colormap):
+        raise ValueError('label value too large.')
+
+    return colormap[label]
+
+
+def vis_segmentation(image, seg_map):
+    """Visualizes input image, segmentation map and overlay view."""
+    plt.figure(figsize=(15, 5))
+    grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
+
+    plt.subplot(grid_spec[0])
+    plt.imshow(image)
+    plt.axis('off')
+    plt.title('input image')
+
+    plt.subplot(grid_spec[1])
+    seg_image = label_to_color_image(seg_map).astype(np.uint8)
+    plt.imshow(seg_image)
+    plt.axis('off')
+    plt.title('segmentation map')
+
+    plt.subplot(grid_spec[2])
+    plt.imshow(image)
+    plt.imshow(seg_image, alpha=0.7)
+    plt.axis('off')
+    plt.title('segmentation overlay')
+
+    unique_labels = np.unique(seg_map)
+    ax = plt.subplot(grid_spec[3])
+    plt.imshow(
+        FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
+    ax.yaxis.tick_right()
+    plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
+    plt.xticks([], [])
+    ax.tick_params(width=0.0)
+    plt.grid('off')
+    plt.show()
+
+
+LABEL_NAMES = np.asarray([
+    'wall', 'background', 'building, edifice', 'sky', 'floor, flooring', 'tree', 'ceiling', 'road, route', 'bed ', 'windowpane, window ', 'grass', 'cabinet', 'sidewalk, pavement', 'person, individual, someone, somebody, mortal, soul', 'earth, ground', 'door, double door', 'table', 'mountain, mount', 'plant, flora, plant life', 'curtain, drape, drapery, mantle, pall', 'chair', 'car, auto, automobile, machine, motorcar', 'water', 'painting, picture', 'sofa, couch, lounge', 'shelf', 'house', 'sea', 'mirror', 'rug, carpet, carpeting', 'field', 'armchair', 'seat', 'fence, fencing', 'desk', 'rock, stone', 'wardrobe, closet, press', 'lamp', 'bathtub, bathing tub, bath, tub', 'railing, rail', 'cushion', 'base, pedestal, stand', 'box', 'column, pillar', 'signboard, sign', 'chest of drawers, chest, bureau, dresser', 'counter', 'sand', 'sink', 'skyscraper', 'fireplace, hearth, open fireplace', 'refrigerator, icebox', 'grandstand, covered stand', 'path', 'stairs, steps', 'runway', 'case, display case, showcase, vitrine', 'pool table, billiard table, snooker table', 'pillow', 'screen door, screen', 'stairway, staircase', 'river', 'bridge, span', 'bookcase', 'blind, screen', 'coffee table, cocktail table', 'toilet, can, commode, crapper, pot, potty, stool, throne', 'flower', 'book', 'hill', 'bench', 'countertop', 'stove, kitchen stove, range, kitchen range, cooking stove', 'palm, palm tree', 'kitchen island', 'computer, computing machine, computing device, data processor, electronic computer, information processing system', 'swivel chair', 'boat', 'bar', 'arcade machine', 'hovel, hut, hutch, shack, shanty', 'bus, autobus, coach, charabanc, double-decker, jitney, motorbus, motorcoach, omnibus, passenger vehicle', 'towel', 'light, light source', 'truck, motortruck', 'tower', 'chandelier, pendant, pendent', 'awning, sunshade, sunblind', 'streetlight, street lamp', 'booth, cubicle, stall, kiosk', 'television receiver, television, television set, tv, tv set, idiot box, boob tube, telly, goggle box', 'airplane, aeroplane, plane', 'dirt track', 'apparel, wearing apparel, dress, clothes', 'pole', 'land, ground, soil', 'bannister, banister, balustrade, balusters, handrail', 'escalator, moving staircase, moving stairway', 'ottoman, pouf, pouffe, puff, hassock', 'bottle', 'buffet, counter, sideboard', 'poster, posting, placard, notice, bill, card', 'stage', 'van', 'ship', 'fountain', 'conveyer belt, conveyor belt, conveyer, conveyor, transporter', 'canopy', 'washer, automatic washer, washing machine', 'plaything, toy', 'swimming pool, swimming bath, natatorium', 'stool', 'barrel, cask', 'basket, handbasket', 'waterfall, falls', 'tent, collapsible shelter', 'bag', 'minibike, motorbike', 'cradle', 'oven', 'ball', 'food, solid food', 'step, stair', 'tank, storage tank', 'trade name, brand name, brand, marque', 'microwave, microwave oven', 'pot, flowerpot', 'animal, animate being, beast, brute, creature, fauna', 'bicycle, bike, wheel, cycle ', 'lake', 'dishwasher, dish washer, dishwashing machine', 'screen, silver screen, projection screen', 'blanket, cover', 'sculpture', 'hood, exhaust hood', 'sconce', 'vase', 'traffic light, traffic signal, stoplight', 'tray', 'ashcan, trash can, garbage can, wastebin, ash bin, ash-bin, ashbin, dustbin, trash barrel, trash bin', 'fan', 'pier, wharf, wharfage, dock', 'crt screen', 'plate', 'monitor, monitoring device', 'bulletin board, notice board', 'shower', 'radiator', 'glass, drinking glass', 'clock', 'flag'
+])
+
+FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
+FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
+
+MODEL_NAME = 'deeplabv3_xception_ade20k_train'
+_DOWNLOAD_URL_PREFIX = 'http://download.tensorflow.org/models/'
+_MODEL_URLS = {
+    'deeplabv3_xception_ade20k_train': 'deeplabv3_xception_ade20k_train_2018_05_29.tar.gz',
+}
+_TARBALL_NAME = 'deeplab_model.tar.gz'
+
+model_dir = tempfile.mkdtemp()
+tf.compat.v1.gfile.MakeDirs(model_dir)
+
+download_path = os.path.join(model_dir, _TARBALL_NAME)
+print('downloading model, this might take a while...')
+urllib.request.urlretrieve(_DOWNLOAD_URL_PREFIX + _MODEL_URLS[MODEL_NAME],
+                           download_path)
+print('download completed! loading DeepLab model...')
+
+MODEL = DeepLabModel(download_path)
+
+print('model loaded successfully!')
+
+MODEL.save('./checkpoints/my_checkpoint')
+
+print('Model saved')
+
+file_name = './im_(149).jpg'
+
+# for name, data in uploaded.items():
+#     with open(file_name, 'wb') as f:
+#         f.write(data)
+#         f.close()
+#         print('saved file ' + name)
+
+orignal_im = Image.open(file_name)
+
+print('running deeplab on image %s...' % file_name)
+resized_im, seg_map = MODEL.run(orignal_im)
+# vis_segmentation(resized_im, seg_map)
+
+w, h = resized_im.size
+print(w, h)
+
+for i in range(h):
+    for j in range(w):
+        if seg_map[i][j] == 1:
+            seg_map[i][j] = 1
+        else:
+            seg_map[i][j] = 0
+
+mask_j=np.repeat(seg_map[...,None],3,axis=2)
+
+mask_j[np.where((mask_j==[1,1,1]).any(axis=2))] = [255,0,0]    # RGB values for Blue color
+
+output_image=mask_j+resized_im
+
+plt.figure(figsize=(15, 5))
+grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
+
+plt.subplot(grid_spec[0])
+plt.imshow(resized_im)
+plt.axis('off')
+plt.title('input image')
+
+plt.subplot(grid_spec[1])
+seg_image = label_to_color_image(seg_map).astype(np.uint8)
+
+plt.imshow(output_image)
+
+plt.axis('off')
+plt.title('coloured image')
+plt.show()
---
+++ server/api/users/routes.py
@@ -0,0 +1,75 @@
+from flask import Blueprint, request
+from bson import json_util
+import json
+from db import db
+import uuid
+from werkzeug import secure_filename
+
+mongoClient = db.get_database()
+
+users_bp = Blueprint(
+    'users_bp', __name__,
+)
+
+
+@users_bp.route('', methods=["POST"])
+def addUser():
+    try:
+        username = request.json["username"]
+        password = request.json["password"]
+
+        user = {
+            'username': username,
+            'password': password
+        }
+
+        user = mongoClient["users"].insert_one(user)
+
+        return {'message': user.acknowledged, 'status': 200}
+
+    except Exception as Argument:
+        print(Argument)
+        return Argument
+
+
+@users_bp.route('', methods=["GET"])
+def getUser():
+    try:
+        username = request.args.to_dict()["username"]
+        user = mongoClient["users"].find_one({'username': username})
+        print(json.loads(json_util.dumps(user)))
+        return {'user': json.loads(json_util.dumps(user)), 'message': 'sucess', 'status': 200}
+
+    except Exception as Argument:
+        print(Argument)
+        return Argument
+
+@users_bp.route('/login', methods=["POST"])
+def loginUser():
+    try:
+        username = request.json["username"]
+        password = request.json["password"]
+
+
+        user = mongoClient["users"].find_one({
+            'username': username,
+            'password': password,
+            })
+
+        return {'user': json.loads(json_util.dumps(user)), 'message': 'sucess', 'status': 200}
+
+    except Exception as Argument:
+        print(Argument)
+        return Argument
+
+
+@users_bp.route('/orignalImage', methods=["POST"])
+def addOrgImage():
+    try:
+        f = request.files['file']
+        username = request.json["username"]
+        fileName = uuid.uuid1()
+                
+    except Exception as Argument:
+        print(Argument)
+        return Argument
