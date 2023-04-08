d810caeabb575d7e5215862f7335e767cb173dd7
Ronnit3012 <ronnit.mirgh@gmail.com>
2023-04-09 02:48:31 +0530

    authentication and verification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home, Profile, Shop} from "./pages/pages";
+import { Home, Login, Signup, Profile, Shop } from "./pages/pages";
 
 function App() {
   return (
@@ -8,8 +8,10 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path='/profile' element={<Profile />} />
-          <Route exact path='/shop' element={<Shop />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/shop" element={<Shop />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
         </Routes>
       </Router>
     </div>
--- client/src/pages/pages.js
+++ client/src/pages/pages.js
@@ -1,5 +1,7 @@
 import Home from "./Home";
 import Profile from "./Profile"
 import Shop from "./Shop"
+import Login from "./Login";
+import Signup from "./Signup";
 
-export { Home, Profile, Shop };
+export { Home, Login, Signup, Profile, Shop };
