82a55c9a6a64949c4ec49f5c46c3adebef3b7eb1
Ronnit3012 <ronnit.mirgh@gmail.com>
2023-04-08 19:11:54 +0530

    added header

---
+++ client/src/components/components.js
@@ -0,0 +1,5 @@
+import Header from "./Header/Header";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+
+export { Header, Navbar, Footer };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Header } from "../components/components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
