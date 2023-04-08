848febefdf154a0a8b39e73e4801343a61874c82
Ronnit3012 <ronnit.mirgh@gmail.com>
2023-04-09 04:06:33 +0530

    added error modal

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { IoLocationOutline, IoPersonOutline } from "react-icons/io5";
 import { FiInfo } from "react-icons/fi";
+import { useDispatch, useSelector } from "react-redux";
+import { setLogout } from "../../store/auth/auth";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+
   return (
     <header id="header" className="border-b-2 sticky">
       <div className="container mx-auto">
@@ -24,12 +29,33 @@ const Header = () => {
                 <p className="text-sm">Need Help</p>
               </div>
             </Link>
-            <Link to="">
-              <div className="text-slate-500 cursor-pointer flex justify-center items-center gap-2">
-                <IoPersonOutline size={20} className="inline" />
-                <p className="text-sm">Sign in / Register</p>
-              </div>
-            </Link>
+            <div className="text-slate-500 cursor-pointer flex justify-center items-center gap-2">
+              <IoPersonOutline size={20} className="inline" />
+              {!user ? (
+                <>
+                  <Link to="/login">
+                    <p className="text-sm">Sign In /</p>
+                  </Link>
+                  <Link to="/signup">
+                    <p className="text-sm">Register</p>
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <Link to={`/profile/${user._id}`}>
+                    <p className="text-sm mr-4">{user.name}</p>
+                  </Link>
+                  <button
+                    onClick={() => {
+                      dispatch(setLogout());
+                      window.location.reload();
+                    }}
+                  >
+                    <p className="text-sm">Logout</p>
+                  </button>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
