2acb322d1c2ae672a14be94e82c6d828d1115bc4
shantanussalvi <shantanussalvi@gmail.com>
2023-04-09 07:46:54 +0530

    suggestion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, Profile, Shop} from "./pages/pages";
+import Suggestion from "./pages/Suggestion";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path='/profile' element={<Profile />} />
           <Route exact path='/shop' element={<Shop />} />
+          <Route exact path='/suggest' element={<Suggestion />} />
         </Routes>
       </Router>
     </div>
