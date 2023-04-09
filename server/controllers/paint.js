bc092d0126e02ca0c133011b48fe86d259777ac2
Ronnit3012 <ronnit.mirgh@gmail.com>
2023-04-09 08:46:12 +0530

    added paint routes

--- server/controllers/paint.js
+++ server/controllers/paint.js
@@ -1,6 +1,7 @@
 import Paint from "../models/Paint.js";
 import axios from "axios";
 import shuffle from "../helpers/shuffle.js";
+import fs from "fs";
 
 const paintWall = async (req, res) => {
     try {
@@ -31,4 +32,29 @@ const getPaints = async (req, res) => {
     }
 }
 
-export { paintWall, getPaints };
+const getPaint = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const paint = await Paint.findById(id);
+        return res.status(200).json(paint);
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+const addPaints = async (req, res) => {
+    const bufferData = fs.readFileSync('./data/data.json');
+    const data = JSON.parse(bufferData.toString());
+
+    try {
+        const response = await Paint.insertMany(data);
+        return res.status(201).json({message: 'Paints added Successfully!'})
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+export { paintWall, getPaints, getPaint, addPaints };
