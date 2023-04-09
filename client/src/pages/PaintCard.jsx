9ebbd9d405fe61b1dfff70c4bcaa2eedf81ca4d2
shantanussalvi <shantanussalvi@gmail.com>
2023-04-09 09:17:25 +0530

    paintcard

--- client/src/pages/PaintCard.jsx
+++ client/src/pages/PaintCard.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const PaintCard = () => {
-  return (
-    <div>PaintCard</div>
-  )
+    const [paints, setPaints] = useState([])
+    useEffect(() => {
+
+        const response = axios({
+            method: 'get',
+            url: '/paint/all',
+        });
+        console.log(response)
+    }, [])
+
+    return (
+        <div
+            className="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700 m-3">
+            <a href="#!">
+                <img
+                    className="rounded-t-lg"
+                    src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
+                    alt="" />
+            </a>
+            <div className="p-6">
+                <h5
+                    className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                    Card title
+                </h5>
+                <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </p>
+                <button
+                    type="button"
+                    class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+                    Try Now
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default PaintCard
